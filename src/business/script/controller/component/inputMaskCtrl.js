acf51faf18720e4c0b1676ddd2b23b1d38659bb7
unknown <15041622@CNHQ-15041622N.sn.suning.ad>
2016-03-02 17:34:06 +0800

    mask

--- src/business/script/controller/component/inputMaskCtrl.js
+++ src/business/script/controller/component/inputMaskCtrl.js
@@ -1,7 +1,14 @@
-let InputMaskCtrl = () => {
-  
+let InputMaskCtrl = ($scope) => {
+  let vm = $scope;
+  vm.validateInput = (inputValList, mask) => {
+    let ret = true;
+    for(let p in inputValList){
+      ret = ret && Number.isInteger(+inputValList[p]);
+    }
+    return ret;
+  };
 };
 
-InputMaskCtrl.$inject = [];
+InputMaskCtrl.$inject = ['$scope'];
 
 export default app => app.controller('InputMaskCtrl', InputMaskCtrl);
