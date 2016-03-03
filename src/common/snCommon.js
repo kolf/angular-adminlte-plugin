ffc63734e2d0afc020088b9b74d4b8b7d9c04ab0
unknown <15041622@CNHQ-15041622N.sn.suning.ad>
2016-03-03 15:51:17 +0800

    http服务从core中移出

--- src/common/snCommon.js
+++ src/common/snCommon.js
@@ -1,13 +1,18 @@
+// 基础组件部分，一般都需要加载
 import angular from 'angular';
 import core from './core';
+import modal from './modal/modal';
+import http from './httpServer/httpServer';
+
+// 功能组件部分，根据需要确认是否加载
 import tree from './tree/tree';
 import markdown from './markdown/markdown';
 import dropdown from './dropdown/dropdown';
-import modal from './modal/modal';
 import preview from './preview/preview';
 import echarts from './echarts/echarts';
 import dateRangePicker from './dateRangePicker/dateRangePicker';
 import inputMask from './inputMask/inputMask';
+import select2 from './select2/select2';
 
 let app = angular.module("sn.common", []);
 
@@ -17,7 +22,7 @@ function injectService(app, ...serviceList) {
   });
 }
 
-injectService(app, core, tree, markdown, dropdown, modal, preview, echarts, 
-  dateRangePicker, inputMask);
+injectService(app, core,  modal, http, tree, markdown, dropdown,preview, echarts, 
+  dateRangePicker, inputMask, select2);
 
 export default app;
